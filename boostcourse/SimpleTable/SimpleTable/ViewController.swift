06d054901df220d75ae3cfbf1818a50b322fe449
U <69714292+dbguswls030@users.noreply.github.com>
2021-07-15 23:53:31 +0900

    Add files via upload

---
+++ boostcourse/SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 유현진 on 2021/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier: String = "cell"
+    
+    let korean: [String] = ["가", "나", "다", "라", "마", "바", "사"]
+    
+    let english: [String] = ["A", "B", "C", "D", "F", "G", "H", "I"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return korean.count
+        case 1:
+            return english.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "한글" : "영어"
+    }
+ 
+
+}
+
