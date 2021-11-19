c083ccd6048570fa147404bdc9144ead8babd88a
U <69714292+dbguswls030@users.noreply.github.com>
2021-11-20 02:37:51 +0900

    Add files via upload

--- TodoList/TodoList/Todo.swift
+++ TodoList/TodoList/Todo.swift
@@ -18,18 +18,20 @@ struct Todo: Codable, Equatable {
     
     mutating func update(isDone: Bool, detail: String, isToday: Bool) {
         // TODO: update 로직 추가
-        
+        self.isDone = isDone
+        self.detail = detail
+        self.isToday =  isToday
     }
     
     static func == (lhs: Self, rhs: Self) -> Bool {
         // TODO: 동등 조건 추가
-        return true
+        return lhs.id == rhs.id
     }
 }
 
 class TodoManager {
     
-    static let shared = TodoManager()
+    static let shared = TodoManager() // 싱글톤 객체
     
     static var lastId: Int = 0
     
@@ -37,21 +39,36 @@ class TodoManager {
     
     func createTodo(detail: String, isToday: Bool) -> Todo {
         //TODO: create로직 추가
-        return Todo(id: 1, isDone: false, detail: "2", isToday: true)
+        let nextId =  TodoManager.lastId + 1
+        TodoManager.lastId = nextId
+        return Todo(id: nextId, isDone: false, detail: detail, isToday: isToday)
     }
     
     func addTodo(_ todo: Todo) {
         //TODO: add로직 추가
+        todos.append(todo)
+        saveTodo()
     }
     
     func deleteTodo(_ todo: Todo) {
         //TODO: delete 로직 추가
-        
+//        todos = todos.filter{ existingTodo in
+//            return existingTodo.id != todo.id
+//        }
+//        위 아래 둘 다 같음
+//        todos = todos.filter{ $0.id != todo.id }
+
+        if let index = todos.firstIndex(of: todo){
+            todos.remove(at: index)
+        }
+        saveTodo()
     }
     
     func updateTodo(_ todo: Todo) {
         //TODO: updatee 로직 추가
-        
+        guard let index = todos.firstIndex(of: todo) else { return }
+        todos[index].update(isDone: todo.isDone, detail: todo.detail, isToday: todo.isToday)
+        saveTodo()
     }
     
     func saveTodo() {
